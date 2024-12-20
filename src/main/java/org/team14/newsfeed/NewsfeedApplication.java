e3895a8d3cfceceb15adaea8c4b3674c391f223d
star-pooh <eunki92@gmail.com>
2024-12-20 15:10:23 +0900

    Init : Create Team14 Newsfeed Project 14조 뉴스 피드 프로젝트 생성

---
+++ src/main/java/org/team14/newsfeed/NewsfeedApplication.java
@@ -0,0 +1,13 @@
+package org.team14.newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NewsfeedApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/team14/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team14.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
