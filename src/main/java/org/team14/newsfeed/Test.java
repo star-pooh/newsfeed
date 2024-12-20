cd7ed599514d087c2e57870213cf59527b21d841
star-pooh <eunki92@gmail.com>
2024-12-20 15:57:20 +0900

    Test ai 코드 리뷰 테스트

---
+++ src/main/java/org/team14/newsfeed/Test.java
@@ -0,0 +1,21 @@
+package org.team14.newsfeed;
+
+public class Test {
+
+  public void test() {
+
+  String[] test = {"", "", "", "", "", "", "", "", ""};
+
+  for (int i = 0; i < 100; i++) {
+    System.out.println(i);
+
+    for (int j = 0; j < 100; j++) {
+      System.out.println(j);
+    }
+  }
+
+  for (int i = 0; i < test.length; i++) {
+    System.out.println(test[i]);
+  }
+  }
+}
