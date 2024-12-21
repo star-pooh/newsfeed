3c83175fff323f098976316fd731880687496dbf
flashback7 <97232474+seoho1@users.noreply.github.com>
2024-12-21 20:13:31 +0900

    feat: create post entity BaseEntity 생성, post 클래스(entity) 생성, JpaAuditing 설정 완료

--- src/main/java/org/team14/newsfeed/NewsfeedApplication.java
+++ src/main/java/org/team14/newsfeed/NewsfeedApplication.java
@@ -2,12 +2,15 @@ package org.team14.newsfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class NewsfeedApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(NewsfeedApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedApplication.class, args);
+    }
 
 }
