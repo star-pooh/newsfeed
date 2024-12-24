b0e6560e3033fa8bcc10dd5b53db3014903193a9
KIRBY-PARK <pkirby0426@gmail.com>
2024-12-24 15:50:21 +0900

    feat : Add validation annotations - 사용자 이름 필드 @NotNull 검증 추가 - 이메일 필드 @Email 인증 추가

--- src/main/java/org/team14/newsfeed/dto/user/UserUpdateRequestDto.java
+++ src/main/java/org/team14/newsfeed/dto/user/UserUpdateRequestDto.java
@@ -1,5 +1,7 @@
 package org.team14.newsfeed.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +11,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserUpdateRequestDto {
     // 사용자 이름
+    @NotNull(message = "Username cannot be null")
     private String username;
 
     // 이메일
+    @Email(message = "Invalid email format")
     private String email;
 }
