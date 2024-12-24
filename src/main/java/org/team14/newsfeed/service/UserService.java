4ca72588d105b77ee0d05a03e8aed98f76c3b9e0
flashback7 <97232474+seoho1@users.noreply.github.com>
2024-12-24 15:24:59 +0900

    feat: add userLogin method in UserService.java 로그인 코드 자체완성, 테스트는 안됨

--- src/main/java/org/team14/newsfeed/service/UserService.java
+++ src/main/java/org/team14/newsfeed/service/UserService.java
@@ -9,52 +9,66 @@ import org.springframework.web.server.ResponseStatusException;
 import org.team14.newsfeed.config.PasswordEncoder;
 import org.team14.newsfeed.dto.user.UserCreateResponseDto;
 import org.team14.newsfeed.entity.User;
+import org.team14.newsfeed.exception.CustomServiceException;
 import org.team14.newsfeed.repository.UserRepository;
+import org.team14.newsfeed.token.JwtUtil;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
 
-  private final PasswordEncoder passwordEncoder;
-  private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
-  /**
-   * 등록된 사용자인지 확인
-   *
-   * @param email 이메일
-   */
-  public void checkRegisteredUser(String email) {
-    User foundUser = this.userRepository.findByEmail(email).orElse(null);
+    /**
+     * 등록된 사용자인지 확인
+     *
+     * @param email 이메일
+     */
+    public void checkRegisteredUser(String email) {
+        User foundUser = this.userRepository.findByEmail(email).orElse(null);
 
-    if (Objects.isNull(foundUser)) {
-      return;
+        if (Objects.isNull(foundUser)) {
+            return;
+        }
+
+        if (foundUser.isDeleted()) {
+            log.error("[UserService.checkRegisteredUser] 탈퇴한 이메일로 가입 시도 : {}", email);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "탈퇴한 이메일로는 가입할 수 없습니다. email : " + email);
+        } else {
+            log.error("[UserService.checkRegisteredUser] 등록된 이메일로 가입 시도 : {}", email);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "이미 등록된 이메일입니다. email : " + email);
+        }
     }
 
-    if (foundUser.isDeleted()) {
-      log.error("[UserService.checkRegisteredUser] 탈퇴한 이메일로 가입 시도 : {}", email);
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-          "탈퇴한 이메일로는 가입할 수 없습니다. email : " + email);
-    } else {
-      log.error("[UserService.checkRegisteredUser] 등록된 이메일로 가입 시도 : {}", email);
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 등록된 이메일입니다. email : " + email);
+    /**
+     * 사용자 생성
+     *
+     * @param username 사용자 이름
+     * @param email    이메일
+     * @param password 비밀번호
+     * @return 등록된 사용자 정보
+     */
+    public UserCreateResponseDto createUser(String username, String email, String password) {
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = User.of(username, email, encodedPassword);
+
+        User savedUser = this.userRepository.save(user);
+
+        return UserCreateResponseDto.of(savedUser);
     }
-  }
 
-  /**
-   * 사용자 생성
-   *
-   * @param username 사용자 이름
-   * @param email    이메일
-   * @param password 비밀번호
-   * @return 등록된 사용자 정보
-   */
-  public UserCreateResponseDto createUser(String username, String email, String password) {
-    String encodedPassword = passwordEncoder.encode(password);
-    User user = User.of(username, email, encodedPassword);
+    public void userLogin(String email, String password) {
+        User findUser = userRepository.findUserByEmailOrElseThrow(email);
 
-    User savedUser = this.userRepository.save(user);
-
-    return UserCreateResponseDto.of(savedUser);
-  }
+        if (!passwordEncoder.matches(password, findUser.getPassword())) {
+            throw new CustomServiceException(getClass().getSimpleName(), HttpStatus.BAD_REQUEST,
+                    "비밀번호가 일치하지 않습니다.");
+        }
+        String token = jwtUtil.createToken(findUser.getUsername());
+    }
 }
