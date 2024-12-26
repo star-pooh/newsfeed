818eed86c5fdb4888141dd4f80e7517a9f3e4a9b
KIRBY-PARK <pkirby0426@gmail.com>
2024-12-26 22:41:19 +0900

    refactor:Change @NotNull to @NotBlank Notation - @NotNull을 @NotBlank로 대체하여 빈 문자열 검증 추가 - 데이터 유효성 검사 조건을 강화하기 위한 변경

--- src/main/java/org/team14/newsfeed/dto/user/UserDeleteRequestDto.java
+++ src/main/java/org/team14/newsfeed/dto/user/UserDeleteRequestDto.java
@@ -1,7 +1,7 @@
 package org.team14.newsfeed.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -10,12 +10,12 @@ import lombok.Getter;
 public class UserDeleteRequestDto {
 
     // 이메일
-    @NotNull(message = "이메일은 필수 입력 값입니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "유효한 이메일 주소를 입력해주세요.")
     private final String email;
 
     // 비밀번호
-    @NotNull(message = "비밀번호는 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Size(min = 8, message = "비밀번호는 최소 8글자 이상이어야 합니다.")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])(?=\\S+$).{8,}$",
             message = "비밀번호는 최소 8자리 이상이어야 하며, 숫자, 문자, 특수문자를 포함해야 합니다.")
